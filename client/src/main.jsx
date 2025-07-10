fc51f8d9d104a7536bea0aa2979377bdf1a8d94a
Robo782 <robertreitz10@yahoo.com>
2025-07-10 13:07:12 +0200

    Initialer Upload mit funktionierender Tailwind/PostCSS-Konfiguration

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,8 @@
+import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./index.css";
+
 
 
 /* Seiten */
--- client/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  plugins: [require("daisyui")],
-  // optional: Standard-Theme auswählen
-  daisyui: { theme: "light" }, // "dark", "cupcake", "lofi", …
-};
